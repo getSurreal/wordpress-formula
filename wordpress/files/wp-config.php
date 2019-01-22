3b154e77934a8bb2058b7f0255416c2b119841d4
bullet <bullet@iMac-2012.local>
2019-01-21 21:16:58 -0600

    fix syntax

--- wordpress/files/wp-config.php
+++ wordpress/files/wp-config.php
@@ -53,11 +53,6 @@ define('DB_COLLATE', '');
  * @since 2.6.0
  */
 define('AUTH_KEY',         'put your unique phrase here');
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'put your unique phrase here');
 define('SECURE_AUTH_KEY',  'put your unique phrase here');
 define('LOGGED_IN_KEY',    'put your unique phrase here');
 define('NONCE_KEY',        'put your unique phrase here');
